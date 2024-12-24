eae03da25bbc4a8881c1c2ddb8c3f873b1424793
slayqueen2019 <nastena9904@bk.ru>
2024-12-24 21:50:39 +0300

    first version of final project

---
+++ blogicum/pages/views.py
@@ -0,0 +1,17 @@
+# View-функции для страниц ошибок
+from django.shortcuts import render
+
+
+def csrf_failure(request, reason=''):
+    """Обработка ошибки 403."""
+    return render(request, 'pages/403csrf.html', status=403)
+
+
+def page_not_found(request, exception):
+    """Обработка ошибки 404."""
+    return render(request, 'pages/404.html', status=404)
+
+
+def server_error(request):
+    """Обработка ошибки 500."""
+    return render(request, 'pages/500.html', status=500)
